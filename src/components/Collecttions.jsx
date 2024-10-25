3bb39d047bbb5ff71c4ca7501c884c5c9d12fbe1
ZyphrAyo <niladrihowladar2@gmail.com>
2024-10-25 16:15:59 +0530

    Banner Done

--- src/components/Collecttions.jsx
+++ src/components/Collecttions.jsx
@@ -9,7 +9,7 @@ const Categories = () => {
   return (
     <div className="flex flex-col items-center w-full bg-white py-12">
       <div className="w-5/6">
-        <h2 className="text-4xl font-bold mb-8 text-left">Categories</h2>
+        <h2 className="text-5xl font-bold mb-8 text-left">Categories</h2>
       </div>
 
       <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-6 gap-4 w-5/6">
